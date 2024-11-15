750309fbea4da1be70ddd746a2bc1793393a17d2
ryshino <ranature1153@gmail.com>
2024-11-15 15:42:19 +0900

    HangmanGameを作成する

---
+++ lib/hangman_game.rb
@@ -0,0 +1,89 @@
+require_relative '../const/questions_const.rb'
+require_relative '../module/input_validator.rb'
+
+class HangmanGame
+  include QuestionsConst
+  include InputValidator
+
+  def initialize
+    @question = QUESTIONS.sample
+    @answer = "_" * @question.size
+    @life = 5
+  end
+
+  def play
+    while @life > 0
+      display_status
+      input = get_input
+
+      next unless valid_input?(input)
+      
+      process_guess(input)
+
+      if solved?
+        display_clear_message
+        break
+      end
+    end
+    display_answer
+  end
+
+  private
+    def display_status
+      puts "ライフ#{@life}"
+      puts "問題'#{@answer}'"      
+    end
+
+    def display_clear_message
+      puts "クリア！"
+    end
+
+    def display_answer
+      puts "正解は#{@question}でした！"
+    end
+
+    def get_input
+      gets.chomp.downcase
+    end
+
+    def valid_input?(input)
+      single_character?(input) && validate_alphabet?(input)
+    end
+
+    def search_quesiton_char(question, input)
+      target_numbers = []
+      question.chars.each_with_index do |question_char, index|
+        target_numbers.push(index) if question_char == input
+      end
+
+      target_numbers
+    end
+
+    def process_guess(input)
+      target_positions = find_matching_positions(input)
+      
+      if target_positions.empty?
+        @life -= 1
+      else
+        update_answer(target_positions, input)
+      end
+    end
+
+    def find_matching_positions(input)
+      target_positions = []
+      @question.chars.each_with_index do |char, index|
+        target_positions.push(index) if char == input
+      end
+      target_positions
+    end
+
+    def update_answer(positions, input)
+      positions.each do |pos|
+        @answer[pos] = input
+      end
+    end
+
+    def solved?
+      !@answer.include?("_")
+    end
+end
