531a83459030d5800225f12dfc3e3fcdb5b7f55b
ryshino <ranature1153@gmail.com>
2024-11-17 16:14:34 +0900

    最初にメソッドとして定義して読みやすくする

--- spec/module/input_validator_spec.rb
+++ spec/module/input_validator_spec.rb
@@ -1,24 +1,32 @@
 require_relative '../../module/input_validator'
 
 RSpec.describe InputValidator do
+  def single_character?(input)
+    InputValidator.single_character?(input)
+  end
+
+  def validate_alphabet?(input)
+    InputValidator.validate_alphabet?(input)
+  end
+
   describe '.single_character?' do
     context '正常系' do
       it '1文字の場合はtrueを返すこと' do
-        expect(InputValidator.single_character?('a')).to be true
+        expect(single_character?('a')).to be true
       end
     end
 
     context '異常系' do
       it '複数文字の場合はfalseを返すこと' do
-        expect(InputValidator.single_character?('ab')).to be false
+        expect(single_character?('ab')).to be false
       end
 
       it '空文字の場合はfalseを返すこと' do
-        expect(InputValidator.single_character?('')).to be false
+        expect(single_character?('')).to be false
       end
 
       it 'エラーメッセージを出力すること' do
-        expect { InputValidator.single_character?('ab') }.to output("入力できるのは1文字です。\n").to_stdout
+        expect { single_character?('ab') }.to output("入力できるのは1文字です。\n").to_stdout
       end
     end
   end
@@ -26,31 +34,31 @@ RSpec.describe InputValidator do
   describe '.validate_alphabet?' do
     context '正常系' do
       it 'アルファベット小文字の場合はtrueを返すこと' do
-        expect(InputValidator.validate_alphabet?('a')).to be true
+        expect(validate_alphabet?('a')).to be true
       end
 
       it 'すべての小文字アルファベットでtrueを返すこと' do
         ('a'..'z').each do |char|
-          expect(InputValidator.validate_alphabet?(char)).to be true
+          expect(validate_alphabet?(char)).to be true
         end
       end
     end
 
     context '異常系' do
       it '数字の場合はfalseを返すこと' do
-        expect(InputValidator.validate_alphabet?('1')).to be false
+        expect(validate_alphabet?('1')).to be false
       end
 
       it '特殊文字の場合はfalseを返すこと' do
-        expect(InputValidator.validate_alphabet?('!')).to be false
+        expect(validate_alphabet?('!')).to be false
       end
 
       it '大文字の場合はfalseを返すこと' do
-        expect(InputValidator.validate_alphabet?('A')).to be false
+        expect(validate_alphabet?('A')).to be false
       end
 
       it 'エラーメッセージを出力すること' do
-        expect { InputValidator.validate_alphabet?('1') }.to output("入力できるのはアルファベットのみです。\n").to_stdout
+        expect { validate_alphabet?('1') }.to output("入力できるのはアルファベットのみです。\n").to_stdout
       end
     end
   end
